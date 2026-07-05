11eccd3461b2c789350d31b877cc3ffdab0dcb2a
Gilcare <gilcare.com@gmail.com>
2026-07-05 12:04:57 +0000

    sentauri.py

--- sentauri.py
+++ sentauri.py
@@ -1,13 +1,27 @@
 import streamlit as st
-import pandas as pd
-from scraper import run_scraper
+from transformers import pipeline
 
-st.set_page_config(page_title="Hospital Price Scraper MVP", layout="wide")
+st.set_page_config(page_title="Pulmonology", layout="wide")
 
-st.title("🏥 US Hospital Procedure Price Scraper (MVP)")
+st.title("🫁Aya", text_alignment = "center")
+st.divider()
+audio_value = st.audio_input("Record Data")
+#if audio_value
 
-st.markdown("""
-Test hospital transparency scrapers for high-demand global procedures.
-Only extracts relevant rows (no full dataset downloads).
-""")
 
+asr = pipeline("automatic-speech-recognition", model="openai/whisper-small")
+text = asr("recording.wav")["text"]
+
+st.write(text)
+
+st.divider()
+# Input Data Via Text
+with st.form("Input Patient's Details", clear_on_submit = True):
+  name= st.text_input("Name")
+  age= st.number_input("Age")
+  gender= st.radio("Gender", ["Female","Male"])
+  hospital_number = st.number_input("Hospital Number")
+  diagnosis = st.text_input("Diagnosis")
+  submit = st.form_submit_button("Submit")
+  if submit:
+    st.success("Submitted☑️")
