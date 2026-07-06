ed20260239ad2f4110bfd9c6b9519b7fb363e274
Gilcare <gilcare.com@gmail.com>
2026-07-06 23:02:08 +0200

    Update sentauri.py

--- sentauri.py
+++ sentauri.py
@@ -21,9 +21,7 @@ model = WhisperModel(
 )
 
 
-st.info("Please say:/nName,/nAge,/nGender,/nHospital Number,/nDiagnosis./n
-Example:
-'John Smith, age 54, male, hospital number 345678, diagnosis asthma.'")
+st.info("Please say:/nName,/nAge,/nGender,/nHospital Number,/nDiagnosis./nExample:'John Smith, age 54, male, hospital number 345678, diagnosis asthma.'")
 
 
 
